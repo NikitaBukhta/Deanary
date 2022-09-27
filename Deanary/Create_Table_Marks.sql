e19937a3102404aea7475a22924dd4d2c3d0bec6
Nikita Bukhta <nikita.bukhta.dev@gmail.com>
2022-09-27 14:52:55 +0300

    [SQL][Deanary/Create_Table_Marks.sql, Deanary/Fill_Table_Marks.sql]
    Created table marks

--- Deanary/Create_Table_Marks.sql
+++ Deanary/Create_Table_Marks.sql
@@ -4,7 +4,12 @@ GO
 
 CREATE TABLE MARKS(
 	id int NOT NULL IDENTITY (1, 1)	PRIMARY KEY,
-	grade ENUM('????????', '?????', '??????????', '??????????') NOT NULL,
-	min_mark_number
-	max_mark_number
+	grade nvarchar(16),
+	min_mark_number int NOT NULL,
+	max_mark_number int NOT NULL,
+	differentiated bit
 );
+
+GO
+
+SELECT * FROM MARKS;
---
+++ Deanary/Fill_Table_Marks.sql
@@ -0,0 +1,9 @@
+USE DEANARYdb
+
+GO
+
+INSERT MARKS VALUES
+	('Bad', 0, 59, 0),
+	('OK', 60, 73, 1),
+	('Good', 74, 89, 1),
+	('Excelent', 90, 100, 1)
